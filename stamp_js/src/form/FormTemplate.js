536ae54c8d7eed1ac287e8ce616cb4d271bb61fd
nittics1991 <bunpabcom@gmail.com>
2019-10-26 02:46:21 +0000

    テンプレート作成中

--- stamp_js/src/form/FormTemplate.js
+++ stamp_js/src/form/FormTemplate.js
@@ -33,23 +33,25 @@ FormTemplate.prototype.get = function(id, params = {}) {
 *   @return string
 */
 FormTemplate.prototype.defineTemplate = function() {
-    this.start = '<div class=".stampjs_form">';
-    this.end = '</div>';
+    //どうするopen/close
+    this.html_select_open = [
+        '<select name="{{name}}">',
+    ];
     
-    this.table_open = '<table class="{{class}}"><thead>';
-    this.table_middle = '</thead><tbody>';
-    this.table_close = '</tbody></table>';
+    this.html_option = [
+        '<option value="{{key}}">{{value}}</option>',
+    ];
+    this.html_input_number = [
+        '<input type="number" name="{{name}}" value="{{value}}"></inpit>',
+    ];
+    this.html_input_radio = [
+        '<input type="radio" name="{{name}}" value="{{value}}" {{checked}}>{{text}}</inpit>',
+    ];
     
-    this.tr_open = '<tr>';
-    this.tr_close = '</tr>';
     
-    this.th = '<th>{{text}}</th>';
     
-    this.td_open = '<td>';
-    this.td_close = '</td>';
     
-    this.td_text = '<td><input type="text" name="{{name}}" value="{{value}}"></input></td>';
-    this.td_number = '<td><input type="number" name="{{name}}" value="{{value}}"></input></td>';
     
-    this.input_radio = '<input type="radio" name="{{name}}" value="{{value}}" checked="{{checked}}">{{text}}</input>';
+    
+    
 };
