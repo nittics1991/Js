274b1e40a8d44331ff20fe8d93d390599e64ed4a
nittics1991 <bunpabcom@gmail.com>
2020-03-15 11:13:14 +0900

    eslint設定中

--- bin/setting_env.sh
+++ bin/setting_env.sh
@@ -8,10 +8,11 @@ sudo apt install nodejs npm
 
 cp ../.npmrc $HOME
 
-GLOBAL_BIN_PATH=$(cat ../.npmrc |grep prefex |sed 's/prefex=//')
+GLOBAL_BIN_PATH=$(cat ../.npmrc |grep prefix |sed 's/prefix=//')
 
 echo "$GLOBAL_BIN_PATH"
 
 mkdir -p "$GLOBAL_BIN_PATH"
 
-export PATH=$PATH:"$GLOBAL_BIN_PATH"
+#閉じれば効果なしだけど
+export PATH=$PATH:"$GLOBAL_BIN_PATH/bin"
